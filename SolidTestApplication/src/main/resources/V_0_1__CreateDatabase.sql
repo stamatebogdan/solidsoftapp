9abc80c36656d0bd562d69d8ef4e957abf990abf
stamatebogdan <78807099+stamatebogdan@users.noreply.github.com>
2021-02-09 17:42:09 +0200

    Add files via upload

---
+++ SolidTestApplication/src/main/resources/V_0_1__CreateDatabase.sql
@@ -0,0 +1,11 @@
+create table telephone_book(
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(20) NOT NULL,
+    Telephone VARCHAR(20) NOT NULL,
+    Email VARCHAR(30) NOT NULL
+);
+
+insert into telephone_book (Name, Telephone, Email)
+values ('Ana', '0732320424', 'ana@mail.ro'),
+       ('Ion', '0732320424', 'ion@mail.ro'),
+       ('Gigi', '0732320424', 'gigi@mail.ro');
